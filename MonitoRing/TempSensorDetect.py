6fb5e8f2fc28c60115f221df2ed9cd53c6bc201f
Mariyan1224 <72885782+Mariyan1224@users.noreply.github.com>
2022-09-12 17:17:26 +0300

    initial

--- MonitoRing/TempSensorDetect.py
+++ MonitoRing/TempSensorDetect.py
@@ -5,13 +5,12 @@ import time
 from time import sleep
 from tkinter.messagebox import YES
 import RPi.GPIO as GPIO
-# An dieser Stelle kann die Pause zwischen den einzelnen Messungen eingestellt werden
+
 sleeptime = 3
-# Der One-Wire EingangsPin wird deklariert und der integrierte PullUp-Widerstand aktiviert
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# Nach Aktivierung des Pull-UP Widerstandes wird gewartet,
-# bis die Kommunikation mit dem DS18B20 Sensor aufgebaut ist
+
 print ("Waiting for initialization")
 base_dir = '/sys/bus/w1/devices/'
 while True:
@@ -23,20 +22,12 @@ while True:
     continue
 device_file = device_folder + '/w1_slave'
 
-# Funktion wird definiert, mit dem der aktuelle Messwert am Sensor ausgelesen werden
-#kann
-def TemperaturMessung():
+emperaturMessung():
  f = open(device_file, 'r')
  lines = f.readlines()
  f.close()
  return lines
-# Zur Initialisierung, wird der Sensor einmal "blind" ausgelesen
 TemperaturMessung()
-# Die Temperaturauswertung: Beim Raspberry Pi werden erkannte One-Wire Slaves im Ordner
-# /sys/bus/w1/devices/ einem eigenen Unterordner zugeordnet. In diesem Ordner befindet sich die Datei w1-slave
-# in dem Die Daten, die über dem One-Wire Bus gesendet wurden gespeichert.
-# In dieser Funktion werden diese Daten analysiert und die Temperatur herausgelesen
-#und ausgegeben
 
 def TemperaturAuswertung():
     lines = TemperaturMessung()
@@ -48,9 +39,6 @@ def TemperaturAuswertung():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
-# Main program loop
-# The measured temperature is output to the console - between the individual measurements
-# is a pause, the length of which can be set with the "sleeptime" variable
 try:
     while True:
         print ("---------------------------------------")
